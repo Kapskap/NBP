ea7068d2ee0192cbc64d75c0aa195c35737538d3
Krzysztof Sulej <kaps@o2.pl>
2024-03-02 14:58:28 +0100

    Usunieto zbednę pliki i wpisy

--- src/Service/SourceFactory.php
+++ src/Service/SourceFactory.php
@@ -6,14 +6,10 @@ use App\Service\Interfaces\SourceFactoryInterface;
 use App\Service\Interfaces\SourceInterface;
 use App\Service\Sources\Nbp;
 use App\Service\Sources\FloatRates;
-use App\Service\CheckDataService;
+
 
 class SourceFactory implements SourceFactoryInterface
 {
-    public function __construct(private  CheckDataService $checkDataService)
-    {
-        $this->checkDataService = $checkDataService;
-    }
     public function createObject(string $source): SourceInterface
     {
         if ($source === 'NBP') {
@@ -27,16 +23,4 @@ class SourceFactory implements SourceFactoryInterface
         throw new \InvalidArgumentException("Unsupported Source");
     }
 }
-//    $sources = ['NBP', 'FloatRates'];
-//
-//    foreach ($sources as $source) {
-//        $source = new createObject($source);    //SourceInterface
-//        $rates = $source->getData();
-//
-//        $effectiveDate = $rates['effectiveDate'];
-//        $sourceId = $rates['sourceId'];
-//    dd($rates, $effectiveDate, $sourceId);
-//
-//        $test = $this->checkDataService->checkDate($effectiveDate, $sourceId, $rates);
-//    dd($test);
-//    }
+
